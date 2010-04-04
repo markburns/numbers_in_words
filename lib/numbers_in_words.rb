e5c8cb83c577ca4bad0cc3fd8cfd8e6dfdf69d06
Mark Burns <markthedeveloper@googlemail.com>
2010-04-04 19:54:57 +0100

    removed unused reference to Japanesenumbers . will add back in if I implement them. refactored code

--- lib/numbers_in_words.rb
+++ lib/numbers_in_words.rb
@@ -9,6 +9,7 @@ module NumbersToWords
     20 => "twenty", 30=>"thirty", 
     40=>"forty", 50=>"fifty", 60 => "sixty", 70=> "seventy", 80=>"eighty", 
     90 => "ninety"}
+  DIGITS= %w[zero one two three four five six seven eight nine]
   POWERS_OF_TEN ={0=>"one", 1 => "ten", 2=> "hundred", 
     3 => "thousand", 6=>"million",
     9=>"billion", 
@@ -53,12 +54,6 @@ module NumbersToWords
       power += 3
     end
   end
-
-  JAPANESE_POWERS_OF_TEN ={0=>"一", 1 => "十", 2=> "百", 
-    3 => "千", 4=>"万",8=>"億", 12=>"兆"}
-
-  DIGITS= %w[zero one two three four five six seven eight nine]
-  JAPANESE_DIGITS= %w[〇 一 二 三 四 五 六 七 八 九]
 
   #returns a hash with powers of ten and their multipliers
   def powers_of_ten 
@@ -114,11 +109,52 @@ module NumbersToWords
     powers = groups.keys.sort.reverse #put in descending order
     powers.each do |power|
       name = POWERS_OF_TEN[power] if language=="English"
-      name = JAPANESE_POWERS_OF_TEN[power] if language=="Japanese"
       digits = groups[power]
       yield power, name, digits
     end
 
+  end
+
+  def english_group group_size
+    number = self.to_i
+    output = ""
+    number.group_words(group_size) do |power, name, digits|
+      if digits > 0
+        prefix = " "
+        #no 'and' between thousands and hundreds
+        prefix << "and " if power == 0 and digits < 100
+        output << prefix + digits.in_english
+        output << prefix + name unless power == 0
+      end
+    end
+    return output
+  end
+
+  def split_googols
+    number = self.to_i
+    output = ""
+    googols = number.to_s[0..(-LENGTH_OF_GOOGOL)].to_i
+    remainder = number.to_s[1-LENGTH_OF_GOOGOL .. -1].to_i
+    output << " " + googols.in_words + " googol" 
+    if remainder > 0
+      prefix = " "
+      prefix << "and " if remainder < 100
+      output << prefix + remainder.in_english
+    end
+    return output
+  end
+
+  def in_googols
+
+    number = self.to_i
+    output = ""
+    output << " " + number.to_s[0..0].to_i.in_english + " googol"
+    remainder = number.to_s[1..-1].to_i
+    prefix = " "
+    prefix << "and " if remainder < 100
+    output << prefix + remainder.in_english if remainder > 0
+
+    return output
   end
 
   def in_english
@@ -133,95 +169,32 @@ module NumbersToWords
       tens = (number/10).round*10 #write the tens
       # e.g. eighty
       output << EXCEPTIONS[tens]
-
       #write the digits
       digit= number - tens
       output << " " + digit.in_english unless digit==0
-
     elsif length == 3
       #e.g. 113 splits into "one hundred" and "thirteen"
-      number.group_words(2) do |power, name, digits|
-        if digits > 0
-          prefix = " "
-          #no and between thousands and hundreds
-          prefix << "and " if power == 0 
-          output << prefix + digits.in_english
-          output << prefix + name if power == 2 #add the hundred
-        end
-      end
-    elsif length < 101 #more than one hundred less than one googol
-      number.group_words(3) do |power, name, digits|
-        if digits > 0
-          prefix = " "
-          prefix << "and " if power==0 and digits < 100
-          output << prefix + digits.in_english
-          output << " " + name  unless power==0
-        end
-      end
-    elsif length > LENGTH_OF_GOOGOL #one googol and larger
-      googols = number.to_s[0..(-LENGTH_OF_GOOGOL)].to_i
-      remainder = number.to_s[1-LENGTH_OF_GOOGOL .. -1].to_i
-      output << " " + googols.in_english + " googol" 
-      if remainder > 0
-        prefix = " "
-        prefix << "and " if remainder < 100
-        output << prefix + remainder.in_english
-      end
+      output << number.english_group(2)
+    elsif length < LENGTH_OF_GOOGOL #more than one hundred less than one googol
+      output << number.english_group(3)
     elsif length == LENGTH_OF_GOOGOL 
-      output << " " + number.to_s[0..0].to_i.in_english + " googol"
-      remainder = number.to_s[1..-1].to_i
-      prefix = " "
-      prefix << "and " if remainder < 100
-      output << prefix + remainder.in_english if remainder > 0
+      output << number.in_googols
+    elsif length > LENGTH_OF_GOOGOL #one googol and larger
+      output << number.split_googols
     end
 
     return output.strip
-  end
-
-  def in_japanese
-    number = self.to_i # make a copy
-    #handle 0-10
-    return DIGITS[number] if number < 10
-    return EXCEPTIONS[number] if EXCEPTIONS[number]
-
-    output = ""
-    if number.to_s.length == 2 #20-99
-
-      #write the tens
-      tens = (number/10).round
-      # e.g. eighty
-      output << JAPANESE_DIGITS[tens] << JAPANESE_POWERS_OF_TEN[1]
-      digit= number - tens
-      output << " " << JAPANESE_DIGITS[digit]
-      #write the digits
-    else #more than one hundred
-      number.group_words 3 do |power, name, digits|
-        if digits > 0
-          prefix = " "
-          prefix << "and " if power==0 and digits < 100
-          output << prefix + digits.in_english
-          output << " " + name  unless power==0
-        end
-      end
-    end
-    return output.strip
-
   end
 
   def in_words language="English"
     case language
     when "English" #allow for I18n
       in_english
-    when "Japanese"
-      in_japanese
     end
   end
 end
 
-class Fixnum
+class Numeric
   include NumbersToWords
 end
-class Bignum
 
-  include NumbersToWords
-end
--- numbers_in_words.gemspec
+++ numbers_in_words.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name = %q{numbers_in_words}
   s.rubyforge_project = "numbers_in_words"
-  s.version = "0.0.3"
+  s.version = "0.0.4"
   s.date = %q{2010-03-31}
   s.authors = ["Mark Burns"]
   s.email = %q{markthedeveloper@googlemail.com}
