7cf6607621bfeb17d8385a6c7c6b48db79981313
Mark Burns <markthedeveloper@gmail.com>
2012-04-10 21:15:26 +0100

    formatting of specs and code and got decimal specs passing

--- lib/numbers.rb
+++ lib/numbers.rb
@@ -1,51 +1,70 @@
 require 'rubygems'
-require 'active_support'
+require 'active_support/core_ext/array'
 module NumbersInWords
+  EXCEPTIONS = {
+    10 => "ten",
+    11 => "eleven",
+    12 => "twelve",
 
-  #handle exceptions to normal numbers
-  EXCEPTIONS = {10=> "ten", 11=>"eleven", 12 => "twelve", 13 => "thirteen", 
-    14=>"fourteen", 15=>"fifteen", 16=>"sixteen" , 
-    17=> "seventeen", 18=> "eighteen", 19=> "nineteen", 
-    20 => "twenty", 30=>"thirty", 
-    40=>"forty", 50=>"fifty", 60 => "sixty", 70=> "seventy", 80=>"eighty", 
-    90 => "ninety"}
+    13 => "thirteen",
+    14 => "fourteen",
+    15 => "fifteen",
+    16 => "sixteen" ,
+    17 => "seventeen",
+    18 => "eighteen",
+    19 => "nineteen",
 
-  DIGITS= %w[zero one two three four five six seven eight nine]
-  POWERS_OF_TEN ={0=>"one", 1 => "ten", 2=> "hundred", 
-    3 => "thousand", 6=>"million",
-    9=>"billion", 
-    12=>"trillion",
-    15=>"quadrillion",
-    18=>"quintillion", 
-    21=>"sextillion",
-    24=>"septillion", 
-    27=>"octillion", 
-    30=>"nonillion", 
-    33=>"decillion",
-    36=>"undecillion", 
-    39=>"duodecillion",
-    42=>"tredecillion", 
-    45=>"quattuordecillion", 
-    48=>"quindecillion",
-    51=>"sexdecillion", 
-    54=>"septendecillion",
-    57=>"octodecillion", 
-    60=>"novemdecillion", 
-    63=>"vigintillion",
-    66=>"unvigintillion", 
-    69=>"duovigintillion", 
-    72=>"trevigintillion",
-    75=>"quattuorvigintillion",
-    78=>"quinvigintillion", 
-    81=>"sexvigintillion",
-    84=>"septenvigintillion", 
-    87=>"octovigintillion",
-    90=>"novemvigintillion", 
-    93=>"trigintillion",
-    96=>"untrigintillion", 
-    99=>"duotrigintillion",
-    100 => "googol"
+    20 => "twenty",
+    30 => "thirty",
+    40 => "forty",
+    50 => "fifty",
+    60 => "sixty",
+    70 => "seventy",
+    80 => "eighty",
+    90 => "ninety"
   }
+
+  DIGITS = %w[zero one two three four five six seven eight nine]
+
+  POWERS_OF_TEN ={
+    0   => "one",
+    1   => "ten",
+    2   => "hundred",
+    3   => "thousand",
+    6   => "million",
+    9   => "billion",
+    12  => "trillion",
+    15  => "quadrillion",
+    18  => "quintillion",
+    21  => "sextillion",
+    24  => "septillion",
+    27  => "octillion",
+    30  => "nonillion",
+    33  => "decillion",
+    36  => "undecillion",
+    39  => "duodecillion",
+    42  => "tredecillion",
+    45  => "quattuordecillion",
+    48  => "quindecillion",
+    51  => "sexdecillion",
+    54  => "septendecillion",
+    57  => "octodecillion",
+    60  => "novemdecillion",
+    63  => "vigintillion",
+    66  => "unvigintillion",
+    69  => "duovigintillion",
+    72  => "trevigintillion",
+    75  => "quattuorvigintillion",
+    78  => "quinvigintillion",
+    81  => "sexvigintillion",
+    84  => "septenvigintillion",
+    87  => "octovigintillion",
+    90  => "novemvigintillion",
+    93  => "trigintillion",
+    96  => "untrigintillion",
+    99  => "duotrigintillion",
+    100 => "googol"
+    }
 
   LENGTH_OF_GOOGOL = 101 #length of the string i.e. one with 100 zeros
 
@@ -58,35 +77,32 @@ module NumbersInWords
 
   def in_english
     #handle 0-9
-    puts "entered in_english method : #{self}"
-    return DIGITS[self] if self.is_a?(Integer) and (0..9).to_a.include? self
+    return DIGITS[self]     if self.is_a?(Integer) and (0..9).to_a.include? self
     #teens etc
     return EXCEPTIONS[self] if self.is_a?(Integer) and EXCEPTIONS[self]
 
     writer = LanguageWriterEnglish.new(self)
-    #decimals
-    in_decimals = writer.decimals()
+
+    in_decimals = writer.decimals
     return in_decimals unless in_decimals.nil?
 
-    number = self.to_i # make a copy
+    number = to_i
 
-    #negative numbers
-    if number < 0
-    puts "before negative call: #{number}"
-    return writer.negative()
-    end
+    return writer.negative() if number < 0
+
     length = number.to_s.length
     output = ""
+
     if length == 2 #20-99
       tens = (number/10).round*10 #write the tens
-      # e.g. eighty
-      output << EXCEPTIONS[tens]
-      #write the digits
-      digit= number - tens
+
+      output << EXCEPTIONS[tens] # e.g. eighty
+
+      digit= number - tens       #write the digits
+
       output << " " + digit.in_english unless digit==0
     else
-      #longer numbers
-      output << writer.write()
+      output << writer.write() #longer numbers
     end
 
     return output.strip
@@ -96,6 +112,7 @@ module NumbersInWords
 
   class LanguageWriter
     attr_accessor :number
+
     def initialize number
       @number = number
     end
@@ -104,6 +121,7 @@ module NumbersInWords
       #1000 and over Numbers are split into groups of three
       groups = NumberGroup.groups_of @number, size
       powers = groups.keys.sort.reverse #put in descending order
+
       powers.each do |power|
         name = POWERS_OF_TEN[power]
         digits = groups[power]
@@ -113,49 +131,51 @@ module NumbersInWords
   end
 
   class LanguageWriterEnglish < LanguageWriter
-    def negative 
-      puts "negative number: #{@number}"
-      return "minus " + (-@number).in_english
+    def negative
+      "minus " + (-@number).in_english
     end
 
-    def write 
-      output = ""
+    def write
       length = @number.to_s.length
-      if length == 3
-        #e.g. 113 splits into "one hundred" and "thirteen"
-        output << write_groups(2)
-      elsif length < LENGTH_OF_GOOGOL #more than one hundred less than one googol
-        output << write_groups(3)
-      elsif length >= LENGTH_OF_GOOGOL 
-        output << write_googols
-      end
-      return output.strip
+      output = if length == 3
+                 #e.g. 113 splits into "one hundred" and "thirteen"
+                 write_groups(2)
+
+                 #more than one hundred less than one googol
+               elsif length < LENGTH_OF_GOOGOL
+                 write_groups(3)
+
+               elsif length >= LENGTH_OF_GOOGOL
+                 write_googols
+
+               end
+      output.strip
     end
 
     def decimals
       int, decimals = NumberGroup.new(@number).split_decimals
-      unless int.nil?
-        out = int.in_english + " point" 
+      if int
+        out = int.in_english + " point "
         decimals.each do |decimal|
-          out << decimal.in_english + " "
+          out << decimal.to_i.in_english + " "
         end
         return out.strip
       end
-      return nil
     end
 
     private
     def write_googols
-      googols, remainder= NumberGroup.new(@number).split_googols
-      output = "" 
-      output << " " + googols.in_words + " googol" 
+      googols, remainder = NumberGroup.new(@number).split_googols
+      output = ""
+      output << " " + googols.in_words + " googol"
       if remainder > 0
         prefix = " "
         prefix << "and " if remainder < 100
         output << prefix + remainder.in_english
       end
-      return output
+      output
     end
+
     def write_groups group
       #e.g. 113 splits into "one hundred" and "thirteen"
       output = ""
@@ -168,12 +188,11 @@ module NumbersInWords
           output << prefix + name unless power == 0
         end
       end
-      return output
+      output
     end
   end
 
   class NumberGroup
-
     include Enumerable
     attr_accessor :number
 
@@ -189,60 +208,53 @@ module NumbersInWords
     end
 
     def groups size
-      #i.e. 1234567 => 7654321     
+      #i.e. 1234567 => 7654321
       groups = @number.to_s.reverse
       #7654321 => 765 432 1
       @array = groups.split("").in_groups_of(size)
       #765 432 1 => 1 432 765
       @array.reverse!
-      #1 432 765 => 1 234 567 
+      #1 432 765 => 1 234 567
       #and turn back into integers
       @array.map! {|group| group.reverse.join("").to_i}
       @array.reverse! # put in ascending order of power of ten
       power = 0
       output = @array.inject({}) do |output, digits|
-        output[power]=digits
-        power+=size
+        output[power] = digits
+        power += size
         output
       end
-      return output
+      output
     end
 
-    def split_decimals 
-
+    def split_decimals
       if @number.is_a? Float
-        int = @number.to_i
+        int     = @number.to_i
         decimal = @number - int
         decimal = decimal.to_s.split(".")[1]
-        digits = decimal.split //
+        digits  = decimal.split //
           #convert to integers array
-          digits.inject!([]) {|out, digit|
+          digits.inject([]) {|out, digit|
           out<< digit.to_i
         }
         return int, digits
-
       end
-      return nil
     end
 
-
-    def NumberGroup.groups_of number, size
-      return NumberGroup.new(number).groups(size)
+    def self.groups_of number, size
+      new(number).groups(size)
     end
 
     def split_googols
-      output = ""
-      googols = @number.to_s[0..(-LENGTH_OF_GOOGOL)].to_i
-      remainder = @number.to_s[1-LENGTH_OF_GOOGOL .. -1].to_i
+      output    = ""
+      googols   = @number.to_s[0  ..  (-LENGTH_OF_GOOGOL)].to_i
+      remainder = @number.to_s[(1-LENGTH_OF_GOOGOL) .. -1].to_i
       return googols, remainder
     end
-
-
   end
 end
 
 
-#Extending the Numeric class to support converting to words
 class Numeric
   include NumbersInWords
 end
--- spec/words_in_numbers_spec.rb
+++ spec/words_in_numbers_spec.rb
@@ -1,127 +1,134 @@
-require 'lib/words'
+require './lib/words'
+
 describe WordsInNumbers do
   it "should do the digits 0-10" do
-    "zero".in_numbers.should == 0
-    "one".in_numbers.should == 1
-    "two".in_numbers.should == 2
-    "three".in_numbers.should == 3
-    "four".in_numbers.should == 4
-    "five".in_numbers.should == 5
-    "six".in_numbers.should == 6
-    "seven".in_numbers.should == 7
-    "eight".in_numbers.should == 8
-    "nine".in_numbers.should == 9
+    "zero"  .in_numbers.should == 0
+    "one"   .in_numbers.should == 1
+    "two"   .in_numbers.should == 2
+    "three" .in_numbers.should == 3
+    "four"  .in_numbers.should == 4
+    "five"  .in_numbers.should == 5
+    "six"   .in_numbers.should == 6
+    "seven" .in_numbers.should == 7
+    "eight" .in_numbers.should == 8
+    "nine"  .in_numbers.should == 9
   end
-  it "should handle numbers for which there is one word" do
-    "ten".in_numbers.should == 10
-    "eleven".in_numbers.should == 11
-    "twelve".in_numbers.should == 12
-    "thirteen".in_numbers.should == 13
-    "fourteen".in_numbers.should == 14
-    "fifteen".in_numbers.should == 15
-    "sixteen".in_numbers.should == 16
-    "seventeen".in_numbers.should == 17
-    "eighteen".in_numbers.should == 18
-    "nineteen".in_numbers.should == 19
-    "twenty".in_numbers.should == 20
-  end
-  it "should handle two word numbers up to 100" do
-    "twenty one".in_numbers.should == 21 
-    "twenty two".in_numbers.should == 22
-    "twenty three".in_numbers.should == 23
-    "twenty four".in_numbers.should == 24
-    "twenty five".in_numbers.should == 25
-    "twenty six".in_numbers.should == 26
-    "twenty seven".in_numbers.should == 27
-    "twenty eight".in_numbers.should == 28
-    "seventy six".in_numbers.should == 76
-    "ninety nine".in_numbers.should == 99
-  end
-  it "should handle hundreds" do
-    "one hundred".in_numbers.should == 100
-    "two hundred".in_numbers.should == 200
-    "three hundred".in_numbers.should == 300
-    "nine hundred".in_numbers.should == 900
-    "one hundred and seventy six".in_numbers.should == 176
-    "one hundred and seventy nine".in_numbers.should == 179
-    "nine hundred and ninety nine".in_numbers.should == 999
 
+  it "should handle numbers for which there is one word" do
+    "ten"       .in_numbers.should == 10
+    "eleven"    .in_numbers.should == 11
+    "twelve"    .in_numbers.should == 12
+    "thirteen"  .in_numbers.should == 13
+    "fourteen"  .in_numbers.should == 14
+    "fifteen"   .in_numbers.should == 15
+    "sixteen"   .in_numbers.should == 16
+    "seventeen" .in_numbers.should == 17
+    "eighteen"  .in_numbers.should == 18
+    "nineteen"  .in_numbers.should == 19
+    "twenty"    .in_numbers.should == 20
   end
+
+  it "should handle two word numbers up to 100" do
+    "twenty one"   .in_numbers.should == 21
+    "twenty two"   .in_numbers.should == 22
+    "twenty three" .in_numbers.should == 23
+    "twenty four"  .in_numbers.should == 24
+    "twenty five"  .in_numbers.should == 25
+    "twenty six"   .in_numbers.should == 26
+    "twenty seven" .in_numbers.should == 27
+    "twenty eight" .in_numbers.should == 28
+    "seventy six"  .in_numbers.should == 76
+    "ninety nine"  .in_numbers.should == 99
+  end
+
+  it "should handle hundreds" do
+    "one hundred"                  .in_numbers.should == 100
+    "two hundred"                  .in_numbers.should == 200
+    "three hundred"                .in_numbers.should == 300
+    "nine hundred"                 .in_numbers.should == 900
+    "one hundred and seventy six"  .in_numbers.should == 176
+    "one hundred and seventy nine" .in_numbers.should == 179
+    "nine hundred and ninety nine" .in_numbers.should == 999
+  end
+
   it "should handle unusual hundreds" do
-    "eleven hundred".in_numbers.should == 1100
-    "twelve hundred".in_numbers.should == 1200
-    "thirteen hundred".in_numbers.should == 1300
-    "fifteen hundred".in_numbers.should == 1500
-    "nineteen hundred".in_numbers.should == 1900
+    "eleven hundred"   .in_numbers.should == 1100
+    "twelve hundred"   .in_numbers.should == 1200
+    "thirteen hundred" .in_numbers.should == 1300
+    "fifteen hundred"  .in_numbers.should == 1500
+    "nineteen hundred" .in_numbers.should == 1900
 
   end
   it "should handle thousands" do
-    "two thousand and one".in_numbers.should ==  2001
-    "one thousand".in_numbers.should ==  1000
-    "two thousand".in_numbers.should ==  2000
-    "three thousand".in_numbers.should ==  3000
-    "nine thousand".in_numbers.should ==  9000
-    "nine thousand two hundred".in_numbers.should ==  9200
-    "nine thousand two hundred and seven".in_numbers.should ==  9207
-    "nine thousand two hundred and ninety seven".in_numbers.should ==  9297
-  end
+    "two thousand and one"                       .in_numbers .should ==  2001
+    "one thousand"                               .in_numbers .should ==  1000
+    "two thousand"                               .in_numbers .should ==  2000
+    "three thousand"                             .in_numbers .should ==  3000
+    "nine thousand"                              .in_numbers .should ==  9000
+    "nine thousand two hundred"                  .in_numbers .should ==  9200
+    "nine thousand two hundred and seven"        .in_numbers .should ==  9207
+    "nine thousand two hundred and ninety seven" .in_numbers .should ==  9297
+    end
 
-  it "should handle larger numbers" do
-    "one million".in_numbers.should ==  1000000
-    "two googol five billion and seventy six".in_numbers.should ==  (2*10**100 + 5*10**9 + 76)
-    "thirty seven million".in_numbers.should == 37 * 10**6
-    "twenty six googol".in_numbers.should == 26 * 10**100
-  end
+    it "should handle larger numbers" do
+    "one million"                                .in_numbers .should ==  1000000
+    "two googol five billion and seventy six"    .in_numbers .should ==  (2*10**100 + 5*10**9 + 76)
+    "thirty seven million"                       .in_numbers .should == 37 * 10**6
+    "twenty six googol"                          .in_numbers .should == 26 * 10**100
+    end
 
   it "should handle numbers in hundreds of thousands etc" do
-    "nine hundred thousand".in_numbers.should == 900000
-    "three hundred and fifty seven thousand".in_numbers.should == 357000
-    "five million three hundred and fifty seven thousand".in_numbers.should == 5357000
-    "nine hundred and ninety nine trillion".in_numbers.should == 999 * 10**12
-  end
-  it "should handle negative numbers" do
-    "minus one".in_numbers.should == -1
-    "minus two googol".in_numbers.should == -2 * 10**100
-    "minus nine hundred and ninety nine trillion".in_numbers.should == -999 * 10**12
-  end
+    "nine hundred thousand"                                            .in_numbers .should == 900000
+    "three hundred and fifty seven thousand"                           .in_numbers .should == 357000
+    "five million three hundred and fifty seven thousand"              .in_numbers .should == 5357000
+    "nine hundred and ninety nine trillion"                            .in_numbers .should == 999 * 10**12
+    end
+    it "should handle negative numbers" do
+    "minus one"                                                        .in_numbers .should == -1
+    "minus two googol"                                                 .in_numbers .should == -2 * 10**100
+    "minus nine hundred and ninety nine trillion"                      .in_numbers .should == -999 * 10**12
+    end
 
-  it "should ignore punctuation and capitalisation" do
-    "Minus one".in_numbers.should == -1
-    "FIVE Million, three hundred and fifty-seVen Thousand".in_numbers.should == 5357000
-    "FIVE,,./';';';[] Million, three hundred and fifty-seVen Thousand".in_numbers.should == 5357000
-   
-  end
+    it "should ignore punctuation and capitalisation" do
+    "Minus one"                                                        .in_numbers .should == -1
+    "FIVE Million, three hundred and fifty-seVen Thousand"             .in_numbers .should == 5357000
+    "FIVE,,./';';';[] Million, three hundred and fifty-seVen Thousand" .in_numbers .should == 5357000
+
+    end
 
   it "should handle decimal points" do
-    "one point one".in_numbers.should == 1.1
+    "one point one"                        .in_numbers .should == 1.1
 
-    "zero point seven six five three four".in_numbers.should == 0.76534
-    "one trillion point six".in_numbers.should == 10**12 + 0.6
+    "zero point seven six five three four" .in_numbers .should == 0.76534
+    "one trillion point six"               .in_numbers .should == 10**12 + 0.6
+
     long_number = <<-NUMBER
-    nine duotrigintillion seven hundred and seventy seven untrigintillion 
-    fifty nine trigintillion one hundred and sixty novemvigintillion 
-    eight hundred and six octovigintillion seven hundred and thirty six 
-    septenvigintillion four hundred and seventy one sexvigintillion 
-    nine hundred and seventy quinvigintillion six hundred and thirty two 
-    quattuorvigintillion eight hundred and twenty seven trevigintillion 
-    eight hundred and thirty six duovigintillion nine hundred and fifty 
-    two unvigintillion seven hundred and ten vigintillion eight hundred and one 
-    novemdecillion nine hundred and forty eight octodecillion seven hundred 
-    and five septendecillion six hundred and eighty three sexdecillion 
-    one hundred and six quindecillion seven hundred and seven quattuordecillion 
-    seven hundred and fifty seven tredecillion four hundred and twenty six 
-    duodecillion seven hundred and ninety five undecillion seven hundred 
-    and forty six decillion eight hundred and thirteen nonillion one hundred 
-    and twenty seven octillion four hundred and sixty five septillion two 
-    hundred and thirty seven sextillion one hundred and thirty nine 
-    quintillion one hundred and fifty three quadrillion forty six 
-    trillion seven hundred and fifty two billion eight hundred and three 
+    nine duotrigintillion seven hundred and seventy seven untrigintillion
+    fifty nine trigintillion one hundred and sixty novemvigintillion
+    eight hundred and six octovigintillion seven hundred and thirty six
+    septenvigintillion four hundred and seventy one sexvigintillion
+    nine hundred and seventy quinvigintillion six hundred and thirty two
+    quattuorvigintillion eight hundred and twenty seven trevigintillion
+    eight hundred and thirty six duovigintillion nine hundred and fifty
+    two unvigintillion seven hundred and ten vigintillion eight hundred and one
+    novemdecillion nine hundred and forty eight octodecillion seven hundred
+    and five septendecillion six hundred and eighty three sexdecillion
+    one hundred and six quindecillion seven hundred and seven quattuordecillion
+    seven hundred and fifty seven tredecillion four hundred and twenty six
+    duodecillion seven hundred and ninety five undecillion seven hundred
+    and forty six decillion eight hundred and thirteen nonillion one hundred
+    and twenty seven octillion four hundred and sixty five septillion two
+    hundred and thirty seven sextillion one hundred and thirty nine
+    quintillion one hundred and fifty three quadrillion forty six
+    trillion seven hundred and fifty two billion eight hundred and three
     million ninety three thousand seven hundred and ninety one point
     eight nine five six four three two one eight nine five six seven eight
     NUMBER
-    long_number.in_numbers.should == 9777059160806736471970632827836952710801948705683106707757426795746813127465237139153046752803093791.89564321895678
-    "seventy five point eight four three two seven six nine four five one eight".in_numbers.should ==
-      75.84327694518
+    long_number.in_numbers.should ==
+      9777059160806736471970632827836952710801948705683106707757426795746813127465237139153046752803093791.89564321895678
+
+    "seventy five point eight four three two seven six nine four five one eight".
+      in_numbers.should == 75.84327694518
   end
 
-end 
+end
