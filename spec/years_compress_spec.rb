fe3e07825b2507764ec6586178cf6976a061c5b9
Dimid Duchovny <dimidd@gmail.com>
2015-10-27 15:17:36 +0200

    spec: fix WS
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- spec/years_compress_spec.rb
+++ spec/years_compress_spec.rb
@@ -4,10 +4,11 @@ describe WordsInNumbers do
   it "should handle years notation" do
     expect("fifteen sixteen seventeen".num_compress) .to eq([15, 16, 17])
     expect("forty nine ninety eight forty seven seventy nine".num_compress) .to eq([49, 98, 47, 79])
-     expect("sixty seven six".num_compress) .to eq([67, 6])
-     expect("one fifty".num_compress).to eq([1, 50])
-     expect("two fifty".num_compress).to eq([2, 50])
-     expect("one three forty seven".num_compress).to eq([1, 3, 47]) 
+    expect("sixty seven six".num_compress) .to eq([67, 6])
+    expect("one fifty".num_compress).to eq([1, 50])
+    expect("two fifty".num_compress).to eq([2, 50])
+    expect("one three forty seven".num_compress).to eq([1, 3, 47])
+    expect("one hundred".num_compress).to eq([100])
   end
 
 end
