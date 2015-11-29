8d6ceb280f7a95418c4bbfe9c4243b22f3bd4d0e
Dimid Duchovny <dimidd@gmail.com>
2015-11-29 18:00:33 +0200

    spec: years: add example with oh
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- spec/years_spec.rb
+++ spec/years_spec.rb
@@ -2,8 +2,8 @@ require './spec/spec_helper'
 
 describe WordsInNumbers do
   it "should handle years notation" do
-    expect("fifteen sixteen seventeen".in_numbers) .to eq(151617)
-    expect("forty nine ninety eight forty seven seventy nine".in_numbers) .to eq(49984779)
+    expect("fifteen sixteen seventeen".in_numbers).to eq(151617)
+    expect("forty nine ninety eight forty seven seventy nine".in_numbers).to eq(49984779)
      expect("sixty seven six".in_numbers) .to eq(676)
      expect("one fifty".in_numbers).to eq(150)
      expect("two fifty".in_numbers).to eq(250)
@@ -11,6 +11,7 @@ describe WordsInNumbers do
      expect("one three forty seven".in_numbers).to eq(1347) 
      expect("one three five point forty seven".in_numbers).to eq(135.47)
 		 expect("one ten sixty three".in_numbers).to eq(11063)
+     expect("one nineteen ten oh five".in_numbers).to eq(1191005)
   end
 
 end
