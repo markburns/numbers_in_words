ae73a42588e9d8681ec45e85208a147808aac77e
Mark Burns <markthedeveloper@gmail.com>
2015-12-01 09:21:47 +0900

    move code-climate test reporter to first line of spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'numbers_in_words'
 require 'numbers_in_words/duck_punch'
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
