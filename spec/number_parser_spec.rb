575b8f4c3f340db37a757df0cc4492c9b923ec24
Mark Burns <markthedeveloper@gmail.com>
2020-08-06 07:29:22 +0100

    remove pending specs

--- spec/number_parser_spec.rb
+++ spec/number_parser_spec.rb
@@ -23,38 +23,6 @@ describe NumbersInWords::NumberParser do
     it { expect(subject).to eq 1600 }
   end
 
-  pending 'with fractions' do
-    context 'with only denominator' do
-      let(:number) { [0.5] }
-      it { expect(subject).to eq 0.5 }
-    end
-
-    context 'with numerator denominator' do
-      let(:number) { [1, 0.5] }
-      it { expect(subject).to eq 0.5 }
-    end
-
-    context 'with multiple denominator' do
-      let(:number) { [3, 0.5] }
-      it { expect(subject).to eq 1.5 }
-    end
-
-    context 'with tens denominator' do
-      let(:number) { [2, 3, 0.5] }
-      pending { expect(subject).to eq 21.5 }
-    end
-
-    context 'with invalid number' do
-      let(:number) { [2, 1, 0.5, 1] }
-      it { expect { subject }.to raise_error NumbersInWords::InvalidNumber }
-    end
-
-    context 'with numerator and denominator' do
-      let(:number) { [2, 0.5] }
-      it { expect(subject).to eq 1.0 }
-    end
-  end
-
   context 'with hundreds' do
     let(:number) { [2, 100, 45] }
 
